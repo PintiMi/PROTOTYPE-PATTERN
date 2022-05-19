80da7ea21fb9d2fb5c1fbb886cb4c8e5afaad8f1
Mihaela <mihaela.pintilei@student.tuiasi.ro>
2022-05-20 00:52:45 +0300

    test commit

--- PrototypePattern/src/factory/FactoryTest.java
+++ PrototypePattern/src/factory/FactoryTest.java
@@ -4,12 +4,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import companies.Apple;
+import companies.Samsung;
+import interfaces.Phone;
+
 class FactoryTest {
 
 	@Test
-	void test() {
-		PhoneFactory test = new PhoneFactory();
-		String output = test.toString();
+	void test() throws CloneNotSupportedException {
+		
+		/**
+		 * creating an Apple product 
+		 * test the set and get methods
+		 */
+		Apple apl = new Apple();
+		apl.setColor("red");
+		apl.getColor();
+		
+		/**
+		 * creating a Samsung product
+		 * test the set and get methods
+		 */
+		Samsung smsg = new Samsung();
+		smsg.setModel("S21");
+		smsg.getModel();
+		
+		/**
+		 * creating a clone
+		 */
+		Phone phoneCopy = apl.makeClone();
+		
+		
+		
 	}
 
 }
